830b97f6e7a7ccc44a4ef61468f180ddcba3d2ea
Tony Mu <tonymu@Tonys-iMac.local>
2017-01-18 22:21:00 +1300

    Added Dev target

--- SaveALittle/AppDelegate.swift
+++ SaveALittle/AppDelegate.swift
@@ -16,7 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        #if DEVELOPMENT
+            debugPrint("SaveALittleDev")
+        #else
+            debugPrint("SaveALittle")
+        #endif
+        
         return true
     }
 
