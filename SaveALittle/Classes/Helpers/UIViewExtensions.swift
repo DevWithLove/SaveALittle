76328c69e6fd6498f880dddcc5d80682444405e1
Tony Mu <tonymu@Tonys-iMac.local>
2017-01-19 19:31:30 +1300

    added UIViewExtensions

---
+++ SaveALittle/Classes/Helpers/UIViewExtensions.swift
@@ -0,0 +1,13 @@
+//
+//  UIViewExtensions.swift
+//  SaveALittle
+//
+//  Created by Tony Mu on 19/01/17.
+//  Copyright © 2017 DevWithLove.com. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+
+}
